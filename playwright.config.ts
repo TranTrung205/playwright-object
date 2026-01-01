2a55ccfa5fcb9faf204fcb744895e7858f363ccf
TranTrung205 <trung@s5tech.co>
2026-01-01 14:48:40 +0700

    clean playwright config for CI screenshot and trace

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,9 +29,10 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    trace: 'retain-on-failure',
   },
-
+  
   /* Configure projects for major browsers */
   projects: [
     // {
