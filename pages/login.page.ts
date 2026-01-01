020c74118b51f0905e03611842f5f7c56ae49247
TranTrung205 <trung@s5tech.co>
2026-01-01 14:26:48 +0700

    base Page, github action

--- pages/login.page.ts
+++ pages/login.page.ts
@@ -1,14 +1,14 @@
 import { Page, Locator } from '@playwright/test';
+import { BasePage } from './base.page';
 
-export class LoginPage {
-  readonly page: Page;
+export class LoginPage extends BasePage {
   readonly usernameInput: Locator;
   readonly passwordInput: Locator;
   readonly loginButton: Locator;
   readonly errorMessage: Locator;
 
-  constructor(page: Page) {
-    this.page = page;
+constructor(page: any) {
+    super(page); 
     this.usernameInput = page.locator('#user-name');
     this.passwordInput = page.locator('#password');
     this.loginButton = page.locator('#login-button');
--- tests/e2e.spec.ts
+++ tests/e2e.spec.ts
@@ -1,15 +1,19 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/login.page';
 import { InventoryPage } from '../pages/inventory.page';
+import { MenuPage } from '../pages/menu.page';
 
-test('Add product to cart thành công', async ({ page }) => {
+test('E2E: Login → Add to cart → Logout', async ({ page }) => {
   const loginPage = new LoginPage(page);
   const inventoryPage = new InventoryPage(page);
+  const menuPage = new MenuPage(page);
 
   await loginPage.goto();
-  await loginPage.login('problem_user', 'secret_sauce');
+  await loginPage.login('standard_user', 'secret_sauce');
 
   await inventoryPage.addToCart();
-
   await expect(inventoryPage.getCartBadge()).toHaveText('1');
+
+  await menuPage.logout();
+  await expect(page).toHaveURL('https://www.saucedemo.com/');
 });
