9ac5d2df6300952dc4f9b5588b81ea1a7a5e25bc
TranTrung205 <trung@s5tech.co>
2026-01-15 00:48:23 +0700

    TC: GET booking ID  API

--- api/clients/booking.api.ts
+++ api/clients/booking.api.ts
@@ -1,7 +1,7 @@
 import { APIRequestContext, expect } from '@playwright/test';
 
 export class BookingApi {
-  constructor(private request: APIRequestContext) {}
+  constructor(private request: APIRequestContext) { }
 
   async createBooking(payload: any) {
     const response = await this.request.post('/booking', {
@@ -12,5 +12,10 @@ export class BookingApi {
 
     const body = await response.json();
     return body;
+  }
+  async getBookingById(bookingId: number) {
+    const response = await this.request.get(`/booking/${bookingId}`);
+    expect(response.status()).toBe(200);
+    return await response.json();
   }
 }
